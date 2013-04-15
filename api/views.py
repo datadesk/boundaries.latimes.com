c0569b75e090a87edd1cf95e5ae5aca2626318e5
palewire <ben.welsh@gmail.com>
2013-04-15 10:26:10 -0700

    Got a basic boundary detail page together. Tho no map yet.

--- api/views.py
+++ api/views.py
@@ -16,7 +16,9 @@ def boundaryset_detail(request, slug):
     """
     All about one BoundarySet
     """
+    obj = get_object_or_404(BoundarySet, slug=slug)
     context = {
-        'obj': get_object_or_404(BoundarySet, slug=slug)
+        'obj': obj,
+        'boundary_list': obj.boundaries.all()
     }
     return render(request, 'api/boundaryset_detail.html', context)
